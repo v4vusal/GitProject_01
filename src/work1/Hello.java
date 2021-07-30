f5a0483ec426af8983dfaa7553d75f53914cc657
Vusal Rahimli <v4vusal@gmail.com>
2021-07-29 23:04:25 -0700

    Added more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Work01 started");
         System.out.println("Created Git Repository");
+        System.out.println("developing hllo");
     }
 }
